d1c7bfaf45ca69fdadd08885af54692696f9ca5b
RojasLeonJuanLuis <rojasleon122@gmail.com>
2017-12-26 17:33:13 -0600

    layout ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Layout from './components/layout/layout'
+import BurgerBuilder from './containers/burger-builder/burger-builder'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>Hello world!</div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     )
   }
